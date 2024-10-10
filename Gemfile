28c1d926efd078f0a70722a6ad810aea8afc816d
Daniel Debogorski <d.debogorski94@gmail.com>
2024-10-10 13:35:26 +0200

    !

--- Gemfile
+++ Gemfile
@@ -60,7 +60,7 @@ group :development do
 end
 
 group :test do
-  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  gem "webdrivers"  # Automatically manages WebDriver binaries
 end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |driver_options|
+    # Add Chrome options here
+    driver_options.add_argument("--disable-gpu")
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--window-size=1400,1400")
+    # Remove or comment out the headless option to see the browser window
+    # driver_options.add_argument("--headless") # Remove this line to run with a visible browser
+  end
 end
